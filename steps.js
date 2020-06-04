06e0f5267e8149cb35b473e0168a662eb79dd4ac
kseroxl <leonowa.kseni@gmail.com>
2020-06-04 08:14:44 +0200

    Instruction

--- steps.js
+++ steps.js
@@ -5,7 +5,7 @@ export const steps = [
    4. Train the model
    5. Wait some seconds
    5. Click "Next"`,
-  `1. After clicking this button you will start a mathematical test
+  `1. After clicking the button "Play" you will start a mathematical test
    2. You will have 8 seconds for each question
    3. Show sign that represents an equated answer to answer the question`,
   `1. Enjoy the game :)`,
